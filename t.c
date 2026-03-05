e4f3720e19872d027c11e1632bb3360f1cde35b0
rob <robdelacruz@gmail.com>
2026-03-05 23:59:18 +0800

    various

--- t.c
+++ t.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <assert.h>
 
 #include <sys/types.h>
 #include <sys/stat.h>
@@ -76,7 +77,95 @@ String make_ipaddr_string(struct sockaddr *sa) {
     return StringNew(bs);
 }
 
+// Nonblocking socket read into buf.
+// Returns  0 for EOF (socket was shutdown)
+//         -1 if error occured (check errno)
+//          1 if socket is still open for receiving data
+int read_sock(int fd, Buffer *buf) {
+    int z;
+    char readbuf[1024];
+    while (1) {
+        z = recv(fd, readbuf, sizeof(readbuf), MSG_DONTWAIT);
+        if (z == 0)
+            return 0;
+        if (z == -1 && errno == EINTR)
+            continue;
+        if (z == -1 && (errno == EAGAIN || errno == EWOULDBLOCK))
+            return 1;
+        if (z == -1) {
+            fprintf(stderr, "recv() on socket %d: %s\n", fd, strerror(errno));
+            return -1; 
+        }
+        assert(z > 0);
+        BufferAppend(buf, readbuf, z);
+    }
+    return 1;
+}
+
+typedef struct _Client {
+    int fd;
+    Buffer buf;
+    struct _Client *next;
+} Client;
+
+Client *client_head = NULL;
+
+Client *add_new_client(int clientfd, Client **head) {
+    Client *client = (Client *) malloc(sizeof(Client));
+    client->fd = clientfd;
+    client->buf = BufferNew(4096);
+    client->next = NULL;
+    if (*head == NULL) {
+        *head = client;
+    } else {
+        Client *node = *head;
+        while (node->next != NULL)
+            node = node->next;
+        node->next = client;
+    }
+    return client;
+}
+
+void remove_client(int clientfd, Client **head) {
+    Client *node = *head;
+    Client *prev = NULL;
+    while (node != NULL) {
+        if (node->fd == clientfd) {
+            if (prev == NULL)
+                *head = node->next;
+            else
+                prev->next = node->next;
+            BufferFree(&node->buf);
+            free(node);
+            break;
+        }
+        prev = node;
+        node = node->next;
+    }
+}
+
+Client *find_client(int clientfd, Client *head) {
+    Client *client = head;
+    while (client != NULL) {
+        if (client->fd == clientfd)
+            break;
+        client = client->next;
+    }
+    return client;
+}
+
+void print_clients() {
+    printf("Clients: ");
+    Client *client = client_head;
+    while (client != NULL) {
+        printf("%d ", client->fd);
+        client = client->next;
+    }
+    printf("\n");
+}
+
 int main(int argc, char *argv[]) {
+    int z;
     char *host = "localhost";
     char *port = "8000";
     if (argc > 1)
@@ -93,6 +182,70 @@ int main(int argc, char *argv[]) {
     String ipaddr = make_ipaddr_string(&sa);
     printf("Listening on %.*s port %s...\n", ipaddr.len, ipaddr.bs, port);
     StringFree(&ipaddr);
+
+    fd_set readfds, writefds;
+    FD_ZERO(&readfds);
+    FD_ZERO(&writefds);
+    int maxfd=0;
+
+    FD_SET(s0, &readfds);
+    maxfd = s0;
+
+    fd_set tmp_readfds, tmp_writefds;
+    while (1) {
+        tmp_readfds = readfds;
+        tmp_writefds = writefds;
+        fprintf(stderr, "select()...\n");
+        z = select(maxfd+1, &tmp_readfds, &tmp_writefds, NULL, NULL);
+        if (z == 0) // timeout
+            continue;
+        if (z == -1 && errno == EINTR)
+            continue;
+        if (z == -1) {
+            fprintf(stderr, "select(): %s\n", strerror(errno));
+            break;
+        }
+
+        for (int i=0; i <= maxfd; i++) {
+            if (FD_ISSET(i, &tmp_readfds)) {
+                // New client connection, ready to receive data from client.
+                if (i == s0) {
+                    socklen_t sa_len = sizeof(struct sockaddr_in);
+                    struct sockaddr_in sa;
+                    int clientfd = accept(s0, (struct sockaddr *) &sa, &sa_len);
+                    if (clientfd == -1) {
+                        fprintf(stderr, "accept(): %s\n", strerror(errno));
+                        continue;
+                    }
+                    fprintf(stderr, "Connected to client %d\n", clientfd);
+                    FD_SET(clientfd, &readfds);
+                    if (clientfd > maxfd)
+                        maxfd = clientfd;
+
+                    add_new_client(clientfd, &client_head);
+                } else {
+                    int clientfd = i;
+                    fprintf(stderr, "Received data from client %d\n", clientfd);
+
+                    Client *client = find_client(clientfd, client_head);
+                    if (client == NULL) {
+                        fprintf(stderr, "Can't find client buffer %d\n", clientfd);
+                        continue;
+                    }
+
+                    z = read_sock(clientfd, &client->buf);
+                    if (z == 0) {
+                        // Client socket closed, close socket and remove client from list.
+                        fprintf(stderr, "Client %d closed\n", clientfd);
+
+                        remove_client(clientfd, &client_head);
+                        FD_CLR(clientfd, &readfds);
+                        close(clientfd);
+                    }
+                }
+            }
+        }
+    }
 
     close(s0);
 
